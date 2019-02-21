688cd127e021600574a5b7f254732b04821dd80f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-21 17:30:54 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.first_name.concat(self.last_name)
+    self.first_name.concat(" " + self.last_name)
   end
 end
